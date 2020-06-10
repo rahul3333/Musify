b3cfa46fa182b6b294eb093a0a093d428648b18b
Rahul Dewan <44262872+rahul3333@users.noreply.github.com>
2020-06-10 05:42:08 +0530

    Music Player Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,35 @@ toggleButton.addEventListener('click', () => {
 function toggleSidebar(){
   document.getElementById("sidebar").classList.toggle('active');
  }
+var id;
+var width=1;
+ $('#play').click(function(e){
+   e.preventDefault();
+   if(document.getElementById('play').src=='http://localhost:8000/images/playbutton.png')
+    {
+      document.getElementById('play').src='/images/pause.png';
+      id=setInterval(progress,1000);
+    }
+    else{
+      document.getElementById('play').src='/images/playbutton.png';
+      clearInterval(id);
+      document.getElementById('go_progress').style.width=width + "%";
+      document.getElementById('tracker').style.marginLeft=width + "%";
+    }
+ })
+
+ function progress(){
+   var tracker=document.getElementById('tracker');
+   var element=document.getElementById('go_progress');
+  if(width>=100){
+    clearInterval(id);
+    element.style.width=0;
+    width=0;
+    document.getElementById('play').src='/images/playbutton.png';
+  }else{
+    width++;
+    
+    tracker.style.marginLeft=width + "%";
+    element.style.width = width + "%";
+  }
+ }
