352cfd147e0691daaea411982d8fd11773ac5275
Rahul Dewan <44262872+rahul3333@users.noreply.github.com>
2020-07-11 16:34:30 +0530

    ss added

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,13 +141,7 @@ var infoArea = document.getElementById( 'file-upload-filename' );
 input.addEventListener( 'change', showFileName );
 
 function showFileName( event ) {
-  
-  // the change event gives us the input it occurred in 
   var input = event.srcElement;
-  
-  // the input has an array of files in the `files` property, each one has a name that you can use. We're just using the name here.
   var fileName = input.files[0].name;
-  
-  // use fileName however fits your app best, i.e. add it into a div
   infoArea.textContent =fileName;
 }
