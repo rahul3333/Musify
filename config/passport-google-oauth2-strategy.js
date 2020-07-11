9342043f7e3bc398eb6826639105674ed8117e98
Rahul Dewan <rahuldewan998.rd@gmail.com>
2020-07-11 21:37:41 +0530

    Update passport-google-oauth2-strategy.js

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -4,8 +4,8 @@ const crypto=require('crypto');
 const User=require('../models/users');
 
 passport.use(new googleStrategy({
-    clientID:"605627827618-lke8kdf59h97sugaap55sk02s883l8un.apps.googleusercontent.com",//Here Enter your client ID
-    clientSecret:"X_EbxG_Z2sPTpDyFdjRjTSfD",//Here Enter your Client Secret
+    clientID:"",//Here Enter your client ID
+    clientSecret:"",//Here Enter your Client Secret
     callbackURL:"http://localhost:8000/users/auth/google/callback"
 },
 
